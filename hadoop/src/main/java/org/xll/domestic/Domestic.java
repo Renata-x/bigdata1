dd715a9d15a87101269348e794e9f6293776a209
乐乐 薛 <2966826355@qq.com>
2024-06-27 23:20:30 +0800

    提交

---
+++ hadoop/src/main/java/org/xll/domestic/Domestic.java
@@ -0,0 +1,20 @@
+package org.xll.domestic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Domestic {
+  private int num;
+  private int age;
+  private String education;
+  private String employment;
+  private int income;
+  private String marital;
+  private String violence;
+}
