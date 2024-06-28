c030850d06f978f099bbb93bc93722bb63ea515b
乐乐 薛 <2966826355@qq.com>
2024-06-28 19:15:15 +0800

    提交

--- hadoop/src/main/java/org/xll/domestic/EducationPercent.java
+++ hadoop/src/main/java/org/xll/domestic/EducationPercent.java
@@ -1,4 +1,32 @@
 package org.xll.domestic;
 
-public class EducationPercent {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EducationPercent implements DBWritable {
+  private String education;
+  private int value;
+
+  @Override
+  public void write(PreparedStatement preparedStatement) throws SQLException {
+    preparedStatement.setString(1,this.education);
+    preparedStatement.setInt(2,this.value);
+  }
+
+  @Override
+  public void readFields(ResultSet resultSet) throws SQLException {
+    this.education = resultSet.getString(1);
+    this.value = resultSet.getInt(2);
+  }
 }
